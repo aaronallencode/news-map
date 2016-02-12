e879cdd8b5e056e11a463f25dbfbe9ded9bf9e28
Aaron Allen <kickinslowly@gmail.com>
2016-02-11 19:51:58 -0800

    Revert "multiple articles"
    This reverts commit 4a1b56ef24773fa797d764ca8d9b8f73710639c0.

--- app.js
+++ app.js
@@ -1,166 +1,135 @@
-// Variables
+//Global Variables
 var latMin = -40;
 var latMax = 60;
 var lonMin = -140;
 var lonMax = 140;
-var myCenter;
 var newsData;
-var newsDataDocs;
+var localNews;
+var localNewsUrl;
+var localNewsUrlLink;
 var randomLocation = "";
 var map;
 var region;
-var newsLocalityLocal;
-var localNewsImgUrl = "";
-var imgUrl = "";
-var article;
-var articleImg = "";
-var articleUrl = "";
-var articleLink = "";
-var article = "";
-var snippets;
 
-//Individual Functions
-var info = function(){
-  grabArticle(randomArticleInt);
-  grabImg(randomArticleInt);
-  setInfoWindow();
-}
-
-var info_x = function(x) { 
-  grabArticle(x);
-  grabImg(x);
-  setInfoWindow();
-}
 
 var northern = function() {
 latMin = 0;
 latMax = 60;
 };
-
 var southern = function() {
   latMin = -40;
   latMax = 0;
 };
 
-var getRandomInt = function() {
-  return Math.floor(Math.random() * 9);
-};
-var randomArticleInt = getRandomInt();
+//Main Function - Initializes map, provides local variables. Contains country function.
 
-
-//Initializes map
 var initMap = function() {
   var randomLat = Math.random() * (latMax - latMin) + latMin;
   var randomLon = Math.random() * (lonMax - lonMin) + lonMin;
-  myCenter = new google.maps.LatLng(randomLat,randomLon);
+  var myCenter = new google.maps.LatLng(randomLat,randomLon);
   var myCenterString = myCenter.toString();
+  
   var mapProp = {
     center: myCenter,
     zoom: 3
   };
 
-  map = new google.maps.Map(document.getElementById("map"),mapProp);
-  
-  console.log(myCenterString);
-}
+    map = new google.maps.Map(document.getElementById("map"),mapProp);
+ 
+ 	console.log(myCenterString);
 
-// gets country
-var getCountry = function() {
-    var geocoder = new google.maps.Geocoder();
-    var latlng = myCenter;
-    geocoder.geocode({"location" : latlng}, function(results, status) {
-      if (status === google.maps.GeocoderStatus.OK) {
-        if (results[1]) {
-          map.setZoom(4);
-    randomLocation = (results[(results.length-1)].formatted_address);
-    newsLocalityLocal = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?fq=glocations:'+'"'+randomLocation+'"'+'=20120101&api-key=85bb8c0adbf93a2930ec27229bc6eb2d:5:74093223';
-    getNewsArticle();
-        
-      } else { 
-    console.log(status);
-    setTimeout(main,1000);
-    }
-    }  else if (status == google.maps.GeocoderStatus.OVER_QUERY_LIMIT) {
-      console.log(status);
-      setTimeout(main, 1000)
-    } else { 
-    console.log(status);
-    setTimeout(main,250);
-    }
-  });
-  }
+ var country = function() {
+  var geocoder = new google.maps.Geocoder();
+  var latlng = myCenter;
 
-//gets news data from nytimes api
-var getNewsArticle = function() {
+  geocoder.geocode({"location" : latlng}, function(results, status) {
+    if (status === google.maps.GeocoderStatus.OK) {
+      if (results[1]) {
+        map.setZoom(4);
+
+  randomLocation = (results[(results.length-1)].formatted_address);
+ 
+  var newsLocalityLocal = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?fq=glocations:'+'"'+randomLocation+'"'+'=20120101&api-key=85bb8c0adbf93a2930ec27229bc6eb2d:5:74093223';
+
+  console.log(randomLocation);
+
 var xhr = new XMLHttpRequest();
 xhr.open("GET", newsLocalityLocal , false);
 xhr.send();
 newsData = JSON.parse(xhr.responseText);
-newsDataDocs = newsData.response.docs;
-  if (newsDataDocs === []){
-    main();
-  }
-console.log(newsData);
-console.log(newsDataDocs);
-console.log(newsLocalityLocal);
-console.log(randomLocation);
+newsDataDocuments = newsData.response.docs;
 
-info();
+var getRandomInt = function() {
+  return Math.floor(Math.random() * 9);
+}
+var randomArticleInt;
+randomArticleInt = getRandomInt();
 
-};
+console.log(randomArticleInt);
+console.log(newsDataDocuments);
 
-//grabs article
-var grabArticle = function(x) {
-  if (newsDataDocs[x]) {
-    article = newsDataDocs[x].snippet;
-    articleUrl = newsDataDocs[x].web_url;
-    articleLink = '"<a href='+articleUrl+'>'+articleUrl+'</a>"';
-  } else {
-    main();
-  }
+localNews = newsDataDocuments[randomArticleInt].snippet;
+
+localNewsUrl = newsDataDocuments[randomArticleInt].web_url;
+
+var localNewsImgUrl = "";
+var imgUrl = "";
+var localNewsImgLink = "";
+
+if (newsDataDocuments[randomArticleInt].multimedia[0]){
+localNewsImgUrl = newsDataDocuments[randomArticleInt].multimedia[0].url;
+imgUrl = 'http://www.nytimes.com/'+ localNewsImgUrl;
+localNewsImgLink = '"<img src='+imgUrl+'>"';
 }
 
-//grabs image
-var grabImg = function(x) {
-    if (newsDataDocs[x].multimedia[0]){
-    localNewsImgUrl = newsDataDocs[x].multimedia[0].url;
-    imgUrl = 'http://www.nytimes.com/'+ localNewsImgUrl;
-    articleImg = '"<img src='+imgUrl+'>"';
-        } else {
-  articleImg = "Sorry! (No picture data for this article)";
+else {
+  localNewsImgLink = "Sorry! (No picture data for this article)";
+}
+
+
+var localNewsUrlLink = '"<a href='+localNewsUrl+'>'+localNewsUrl+'</a>"';
+
+console.log(localNewsImgLink);
+console.log(localNews);
+console.log(localNewsUrl);
+console.log(xhr.status);
+console.log(xhr.statusText);
+console.log(newsLocalityLocal);
+
+        if(localNews === "") {
+          console.log(status);
+          initMap();
         }
-    }
 
-//sets infowindow
-var setInfoWindow = function() {
-
-    var infowindow = new google.maps.InfoWindow({
+var infowindow = new google.maps.InfoWindow({
     position:myCenter,
   });
-    
-    infowindow.setContent(article + "<br>" + "<br>" + articleLink + "<br>" + "<br>" + articleImg + "<br>" + "<br>" + "Country:" + "  " + randomLocation);
-    infowindow.open(map);
-    console.log(infowindow);
-  }
 
-//Main Function 
-var main = function() {
-  initMap();
-  getCountry(getNewsArticle);
+  infowindow.setContent(localNews + "<br>" + "<br>" + localNewsUrlLink + "<br>" + "<br>" + localNewsImgLink + "<br>" + "<br>" + "Country:" + "  " + randomLocation);
+  infowindow.open(map);
+  
+    } 
+    else { 
+  console.log(status);
+  setTimeout(initMap,500);
+  }
+ } 
+  else { 
+  console.log(status);
+  setTimeout(initMap,500);
+  }
+});
 }
 
-//event listener, runs main function on window load
-google.maps.event.addDomListener(window, 'load', main);
+country();
+
+}
+
+
+google.maps.event.addDomListener(window, 'load', initMap);
 
 
 
-var closeInfoWindow = function() {
-    infoWindow.close();
-};
-
-google.maps.event.addListener(map, 'click', closeInfoWindow);
-
-//Experimental functions below
 
 
 
