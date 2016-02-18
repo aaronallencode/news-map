b5497da072d0bd385a008941e11c9f6c0a07e9ef
Aaron Allen <kickinslowly@gmail.com>
2016-02-18 11:50:05 -0800

    map/filter + button layout
    - now uses map and filter functions to produce workable arrays and
    objects.
    - now has buttons that filter
    - now closes old infowindows upon opening a new one
    - added style to buttons and placed them in proper positions.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var latMin = -40;
 var latMax = 60;
 var lonMin = -140;
 var lonMax = 140;
+var spin_button = '<button onClick="main()"  style="height:auto;width:50%;position:absolute;top:75%;left:40%;">Spin to another Random Location</button>';
 var myCenter;
 var newsData;
 var newsDataDocs;
@@ -13,25 +14,20 @@ var newsLocalityLocal;
 var localNewsImgUrl = "";
 var imgUrl = "";
 var article;
+var articles;
+var articlesSports;
+var articlesForeign;
+var articlesBusiness;
+var articlesEditorial;
 var articleImg = "";
 var articleUrl = "";
 var articleLink = "";
-var article = "";
-var snippets;
+var total;
+var section;
+var infowindow;
 
-//Individual Functions
-//defines random article/img to grab and then sets window
-var info = function(){
-  grabArticle(randomArticleInt);
-  grabImg(randomArticleInt);
-  setInfoWindow();
-}
-//defines specific article/img to grab and then sets window
-var info_x = function(x) { 
-  grabArticle(x);
-  grabImg(x);
-  setInfoWindow();
-}
+//Individual Functions All Listed Below
+
 //sets lat/lon
 var northern = function() {
 latMin = 0;
@@ -43,11 +39,11 @@ var southern = function() {
   latMax = 0;
 };
 
+//creates random integer to be used later to grab an article
 var getRandomInt = function() {
   return Math.floor(Math.random() * 9);
 };
 var randomArticleInt = getRandomInt();
-
 
 //Initializes map
 var initMap = function() {
@@ -62,6 +58,11 @@ var initMap = function() {
 
   map = new google.maps.Map(document.getElementById("map"),mapProp);
   
+
+  infowindow = new google.maps.InfoWindow({
+    position:myCenter,
+  });
+
   console.log(myCenterString);
 }
 
@@ -91,7 +92,7 @@ var getCountry = function() {
   });
   }
 
-//gets news data from nytimes 
+//gets news data from nytimes and uses map/filter to produce readable arrays
 var getNewsArticle = function() {
 var xhr = new XMLHttpRequest();
 xhr.open("GET", newsLocalityLocal , false);
@@ -106,16 +107,110 @@ console.log(newsDataDocs);
 console.log(newsLocalityLocal);
 console.log(randomLocation);
 
-info();
+articles = newsDataDocs.map(function(article){
+      return {
+          section: article.news_desk,
+          snippet: article.snippet, 
+          url: article.web_url, 
+          media: article.multimedia.map(function(img){
+            return img.url;
+          })};
+                })
 
+articlesForeign = articles.filter(function(article){
+  return article.section === "Foreign";
+})
+
+articlesBusiness = articles.filter(function(article){
+  return article.section === "Business";
+})
+
+articlesEditorial = articles.filter(function(article){
+  return article.section === "Editorial";
+})
+
+info();
 };
+
+//grabs foreign article
+var grabForeignArticle = function(){
+  if (articlesForeign) {
+    article = articlesForeign[0].snippet;
+    articleUrl = articlesForeign[0].url;
+    articleLink = '"<a href='+articleUrl+'>'+articleUrl+'</a>"';
+    section = articlesForeign[0].section;
+  } else {
+    alert("No Foreign Articles Found, try another category/article or spin to a new location!");
+  }
+}
+
+//grabs foreign img
+var grabForeignImg = function() {
+    if (articlesForeign[0].media[0]){
+    localNewsImgUrl = articlesForeign[0].media[0];
+    imgUrl = 'http://www.nytimes.com/'+ localNewsImgUrl;
+    articleImg = '"<img src='+imgUrl+'>"';
+        } else {
+  articleImg = "Sorry! (No picture data for this article)";
+        }
+    }
+
+//grabs business article
+var grabBusinessArticle = function(){
+  if (articlesBusiness[0]) {
+    article = articlesBusiness[0].snippet;
+    articleUrl = articlesBusiness[0].url;
+    articleLink = '"<a href='+articleUrl+'>'+articleUrl+'</a>"';
+    section = articlesBusiness[0].section;
+  } else {
+    alert("No Business Articles Found, try another category/article or spin to a new location!");
+  }
+}
+
+//grabs business img
+var grabBusinessImg = function() {
+    if (articlesBusiness[0]){
+    localNewsImgUrl = articlesBusiness[0].media[0];
+    imgUrl = 'http://www.nytimes.com/'+ localNewsImgUrl;
+    articleImg = '"<img src='+imgUrl+'>"';
+        } else {
+  articleImg = "Sorry! (No picture data for this article)";
+        }
+    }
+
+//grabs editorial article
+var grabEditorialArticle = function(){
+  if (articlesEditorial[0]) {
+    article = articlesEditorial[0].snippet;
+    articleUrl = articlesEditorial[0].url;
+    articleLink = '"<a href='+articleUrl+'>'+articleUrl+'</a>"';
+    section = articlesEditorial[0].section;
+  } else {
+    alert("No Editorial Articles Found, try another category/article or spin to a new location!");
+  }
+}
+
+var grabEditorialImg = function() {
+    if (articlesEditorial[0]){
+    localNewsImgUrl = articlesEditorial[0].media[0];
+    imgUrl = 'http://www.nytimes.com/'+ localNewsImgUrl;
+    articleImg = '"<img src='+imgUrl+'>"';
+        } else {
+  articleImg = "Sorry! (No picture data for this article)";
+        }
+    }
 
 //grabs article
 var grabArticle = function(x) {
-  if (newsDataDocs[x]) {
-    article = newsDataDocs[x].snippet;
-    articleUrl = newsDataDocs[x].web_url;
+
+  if (articles[0]) {
+    article = articles[x].snippet;
+    articleUrl = articles[x].url;
     articleLink = '"<a href='+articleUrl+'>'+articleUrl+'</a>"';
+    section = articles[x].section;
+    if (section === null){
+      section = "Not Available"
+    }
   } else {
     main();
   }
@@ -123,8 +218,8 @@ var grabArticle = function(x) {
 
 //grabs image
 var grabImg = function(x) {
-    if (newsDataDocs[x].multimedia[0]){
-    localNewsImgUrl = newsDataDocs[x].multimedia[0].url;
+    if (articles[x].media[0]){
+    localNewsImgUrl = articles[x].media[0];
     imgUrl = 'http://www.nytimes.com/'+ localNewsImgUrl;
     articleImg = '"<img src='+imgUrl+'>"';
         } else {
@@ -134,18 +229,54 @@ var grabImg = function(x) {
 
 //sets infowindow
 var setInfoWindow = function() {
-
-    var infowindow = new google.maps.InfoWindow({
-    position:myCenter,
-  });
-    
-    infowindow.setContent(article + "<br>" + "<br>" + articleLink + "<br>" + "<br>" + articleImg + "<br>" + "<br>" + "Country:" + "  " + randomLocation);
+    infowindow.close();
+    infowindow.setContent(article + "<br>" + "<br>" + articleLink + "<br>" + "<br>" + articleImg + "<br>" + "<br>" + "Country:" + " " + randomLocation + "<br>" + "<br>" + "Section:" + " " + section + spin_button);
     infowindow.open(map);
-    console.log(infowindow);
   }
+
+//defines random article/img to grab and then sets window
+var info = function(){
+  grabArticle(randomArticleInt);
+  grabImg(randomArticleInt);
+  setInfoWindow();
+}
+
+//defines specific article/img to grab and then sets window
+var info_x = function(x) { 
+  grabArticle(x);
+  grabImg(x);
+  setInfoWindow();
+}
+
+//grabs sports information then sets window
+var infoSports = function() { 
+    grabSports();
+}
+
+//grabs foreign information then sets window
+var infoForeign = function() {
+  grabForeignArticle();
+  grabForeignImg();
+  setInfoWindow();
+}
+
+//grabs business information then sets window
+var infoBusiness = function() {
+   grabBusinessImg();
+   grabBusinessArticle();
+   setInfoWindow();
+}
+
+//grabs editorial information then sets window
+var infoEditorial = function() {
+  grabEditorialArticle();
+  grabEditorialImg();
+  setInfoWindow();
+}
 
 //Main Function 
 var main = function() {
+  'use strict';
   initMap();
   getCountry(getNewsArticle);
 }
@@ -155,13 +286,6 @@ google.maps.event.addDomListener(window, 'load', main);
 
 
 
-var closeInfoWindow = function() {
-    infoWindow.close();
-};
-
-google.maps.event.addListener(map, 'click', closeInfoWindow);
-
-//Experimental functions below
 
 
 
